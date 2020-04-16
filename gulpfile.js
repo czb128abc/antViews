be0fc8302383c0db9581d4a8ab4063a7b943a88b
Chen <Chen@Chens-Mac-mini.local>
2020-04-17 02:42:56 +0800

    update gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -4,7 +4,24 @@ const clean = require('del');
 // const ts = require('gulp-typescript');
 const merge = require('merge2');
 
-// const tsProject = ts.createProject('./tsconfig.json');
+const config = {
+  // allowJs: true,
+  include: ['src/**/*.js', 'src/**/*.jsx'],
+  compilerOptions: {
+    sourceMap: true,
+    noImplicitAny: false, // 是否允许属性定义为any
+    esModuleInterop: true,
+    allowSyntheticDefaultImports: true,
+    moduleResolution: 'node', // module === "AMD" or "System" or "ES6" ? "Classic" : "Node". Determine how modules get resolved
+    // "module": "es2015",
+    target: 'es2015',
+    jsx: 'react',
+  },
+  exclude: ['node_modules', 'lib', 'guide', 'docs', 'site', 'dist', 'es'],
+};
+console.log('...config', config);
+
+// const tsProject = ts.createProject(config);
 const ESDIR = './es';
 const LIBDIR = './lib';
 
@@ -24,7 +41,13 @@ function moveLess(dir) {
 
 function compileTs() {
   // return tsProject.src().pipe(tsProject());
-  return gulp.src('./src/**/*.js');
+  return gulp.src(['./src/**/*.js', './src/**/*.jsx'], {
+    debug: true,
+  });
+  // console.log('===compileTs==');
+  // const sream = tsProject.src().pipe(tsProject()).js;
+  // console.log('===compileTs==');
+  // return sream;
 }
 
 function babelConfig(moduleType) {
@@ -55,8 +78,8 @@ gulp.task(
   'lib',
   gulp.series('clean', () => {
     const tsSream = compileTs();
-    const jsStream = tsSream.pipe(babel(babelConfig('commonjs'))).pipe(gulp.dest(ESDIR));
-    const cssStream = moveLess(ESDIR); // 处理css流
+    const jsStream = tsSream.pipe(babel(babelConfig('commonjs'))).pipe(gulp.dest(LIBDIR));
+    const cssStream = moveLess(LIBDIR); // 处理css流
     return merge(jsStream, cssStream);
   }),
 );
