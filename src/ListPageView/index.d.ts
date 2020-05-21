2abeb02a4a72fb8de6e23d7c0a4f3c9172bb7751
czb128abc <czb128abc@163.com>
2020-05-21 10:31:28 +0800

    :zap:“:racehorse:perf：改善性能 add d.ts

--- src/ListPageView/index.d.ts
+++ src/ListPageView/index.d.ts
@@ -2,11 +2,76 @@ import * as React from 'react';
 
 declare class AbstractListPageView extends React.Component {
   setConfig(config: any);
+
+  getDetailDefaultProps(): object;
+
+  getDetailCommonProps(): object;
+
+  saveOrUpdate(): void;
+
+  /**
+   *
+   * @param item
+   */
+  del(item: any): void;
+
+  /**
+   *
+   * @param condition
+   * @param dispatchType
+   */
+  baseSearch(condition: any, dispatchType: any): void;
+
+  /**
+   *
+   * @param condition
+   */
+  search(condition: any): void;
+
+  /**
+   *
+   * @param record
+   * @param callback
+   */
+  queryDetail(record: any, callback: any): void;
+
+  calcSaveOrUpdateParams(values): object;
+
+  calcSearchParams(values): object;
+
+  calcQueryDetailParams(values): object;
+
+  isPopupDisplayEditType(): boolean;
+
+  toAddIsAlignWithTitle(): boolean;
 }
 
 declare class ListPageView extends AbstractListPageView {
   setSelectRecord(selectRecord, callback: void): void;
-  rendMain();
+
+  showAddView(): void;
+
+  showEditView(item): void;
+
+  rendContent(): React.ReactNode;
+
+  rendPopupDetail(): React.ReactNode;
+
+  getListItemActionList(item): any;
+
+  calcListTableColumns(tableColumnsMap): any;
+
+  getListTableColumns(): any;
+
+  rendListItemView(item): React.ReactNode;
+
+  rendListOrTableView(): React.ReactNode;
+
+  rendTitleCardExtra(): React.ReactNode;
+
+  rendSearchFilter(): React.ReactNode;
+
+  rendMain(): React.ReactNode;
 }
 
 export function bindReduxAntFormProps(pageConfig: object, connectCallback: Function): any;
